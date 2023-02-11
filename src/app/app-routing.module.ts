f343f566d515a5b574024239e93107a507cf0bfc
Rogério de F. Pereira <rogeriorfp@outlook.com>
2023-02-10 22:42:33 -0300

    Registra rotas da aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateRoomComponent } from './rooms/create-room/create-room.component';
+import { RoomDetailsComponent } from './rooms/room-details/room-details.component';
+import { RoomListComponent } from './rooms/room-list/room-list.component';
+import { UpdateRoomComponent } from './rooms/update-room/update-room.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'details/:id', component: RoomDetailsComponent},
+  {path: 'update/:id', component: UpdateRoomComponent},
+  {path: 'add', component: CreateRoomComponent},
+  {path:'rooms', component: RoomListComponent},
+  {path: '', redirectTo: 'rooms', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
