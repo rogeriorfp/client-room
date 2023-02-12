e06d9cd08f8315c934b3ae5a877329013153ac45
Rogério de F. Pereira <rogeriorfp@outlook.com>
2023-02-11 23:30:43 -0300

    Implementa a listagem de detalhes da sala

--- src/app/rooms/room-details/room-details.component.ts
+++ src/app/rooms/room-details/room-details.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RoomService } from 'src/app/services/room.service';
+import { Room } from '../room';
 
 @Component({
   selector: 'app-room-details',
   templateUrl: './room-details.component.html',
   styleUrls: ['./room-details.component.css']
 })
-export class RoomDetailsComponent {
+export class RoomDetailsComponent implements OnInit{
+  id!:number
+  room!:Room
 
+
+
+  constructor(private roomService:RoomService, private route:ActivatedRoute, private router:Router){}
+  ngOnInit(): void {
+    this.room = new Room();
+    this.id = this.route.snapshot.params['id'];
+    this.roomService.getRoom(this.id)
+      .subscribe(data => {
+        console.log(data); 
+        this.room = data
+      }, 
+      error => console.error(error));
+  }
+  list(){
+    this.router.navigate(['/rooms']);
+  }
 }
