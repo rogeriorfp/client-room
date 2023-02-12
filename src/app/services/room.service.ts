60e25b30fa6976d7cb9e8c52d6b7ca62f45cbc7a
Rogério de F. Pereira <rogeriorfp@outlook.com>
2023-02-11 23:34:53 -0300

    Atualiza RoomService
    Acrescenta um pipe que converte data de string para o tipo Date

--- src/app/services/room.service.ts
+++ src/app/services/room.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Room } from '../rooms/room';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 const baseUrl = 'http://localhost:8080/api/v1';
 
@@ -9,22 +9,29 @@ const baseUrl = 'http://localhost:8080/api/v1';
   providedIn: 'root'
 })
 export class RoomService {
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  createRoom(room:Room):Observable<Room>{
-    return this.httpClient.post<Room>(`${baseUrl}/rooms`,room);
+  createRoom(room: Room): Observable<Room> {
+    return this.httpClient.post<Room>(`${baseUrl}/rooms`, room);
   }
 
-  getRoom(roomId:number){
-    return this.httpClient.get<Room>(`${baseUrl}/rooms/${roomId}`)
+  getRoom(roomId: number) {
+    return this.httpClient.get<Room>(`${baseUrl}/rooms/${roomId}`).pipe(
+      map((data) => {
+        data.date = new Date(data.date);
+        data.startHour = new Date(data.startHour);
+        data.endHour = new Date(data.endHour);
+        return data;
+      })
+    )
   }
-  getRoomList():Observable<Room[]> {
+  getRoomList(): Observable<Room[]> {
     return this.httpClient.get<Room[]>(`${baseUrl}/rooms`);
   }
-  updateRoom(roomId:number, room:Room){
-    return this.httpClient.put(`${baseUrl}/rooms/${roomId}`,room)
+  updateRoom(roomId: number, room: Room) {
+    return this.httpClient.put(`${baseUrl}/rooms/${roomId}`, room)
   }
-  deleteRoom(roomId:number){
+  deleteRoom(roomId: number) {
     return this.httpClient.delete(`${baseUrl}/rooms/${roomId}`);
   }
 }
